f73fa507da37ca20b6903d9abb2305b6a67d735b
BernardoS <bernardo.sfs27@gmail.com>
2021-06-04 19:48:26 -0300

    Adjusment

--- BookList.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BookList.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- BookList.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookList.API.WebApiApplication" Language="C#" %>
